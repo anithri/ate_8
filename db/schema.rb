1a2f51ca46c37e400c3060c76919e8600c656522
Scott M Parrish <anithri@gmail.com>
2018-12-08 12:56:59 -0700

    active surus hstore

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_224521) do
+ActiveRecord::Schema.define(version: 2018_12_08_195243) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
-
-  create_table "entries", id: :serial, force: :cascade do |t|
-    t.integer "tunnel_log_id"
-    t.integer "task_code_id"
-    t.integer "lab_tech_id"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.boolean "sticky"
-    t.string "projectable_type", limit: 255
-    t.integer "projectable_id"
-    t.text "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean "rework", default: false
-    t.integer "employee_id"
-    t.index ["tunnel_log_id"], name: "index_entries_on_tunnel_log_id"
-  end
-
-  create_table "tunnel_logs", id: :serial, force: :cascade do |t|
-    t.string "slug", limit: 255
-    t.integer "fixed_order"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean "retired", default: false
-  end
 
 end

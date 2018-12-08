8231c2b19844f509b23579ab32d51b19cf02b825
Scott M Parrish <anithri@gmail.com>
2018-12-08 13:11:40 -0700

    annotate and seed User

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+USER_SEEDS = {
+  batman:  'bwayne@wayne-corp.com',
+  robin:   'tdrake@wayne-manor.com',
+  batgirl: 'bgordon@gordon.org',
+  alfred:  'apennyworth@wayne-manor.com'
+}
+
+USER_SEEDS.each_pair do |(name, email)|
+  User.find_or_create_by(name: name.to_s.titleize, email: email)
+end
